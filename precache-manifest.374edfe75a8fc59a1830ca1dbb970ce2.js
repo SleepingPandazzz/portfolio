6a9b4f097b788571a52c213c7bc4870d0e09083e
Tingting He <tingtinghe@Tingtings-MacBook-Pro-2.local>
2022-03-22 20:03:38 -0700

    Updates

---
+++ precache-manifest.374edfe75a8fc59a1830ca1dbb970ce2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bb00ed587511fac7949",
+    "url": "/static/css/main.ecf7dc2b.chunk.css"
+  },
+  {
+    "revision": "4bb00ed587511fac7949",
+    "url": "/static/js/main.c4664c73.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "14b48fe9392412509670",
+    "url": "/static/css/2.1f1b255e.chunk.css"
+  },
+  {
+    "revision": "14b48fe9392412509670",
+    "url": "/static/js/2.83b55812.chunk.js"
+  },
+  {
+    "revision": "cb3913666f29c38fe8de9c620f749522",
+    "url": "/static/media/kabs.cb391366.jpg"
+  },
+  {
+    "revision": "415230e6882d8343ba1ff8eb4b7899eb",
+    "url": "/static/media/pl4.415230e6.jpg"
+  },
+  {
+    "revision": "6c2634c23d0dd70e4b20f282f67f1578",
+    "url": "/static/media/asteroidGame.6c2634c2.jpg"
+  },
+  {
+    "revision": "14c5eaa465acba59e7974806e53d4a85",
+    "url": "/static/media/spaceGame.14c5eaa4.mp4"
+  },
+  {
+    "revision": "fc9b2fb347e253fa7b2b9e9e6b0b01b6",
+    "url": "/static/media/3DGame.fc9b2fb3.jpg"
+  },
+  {
+    "revision": "4c30588c4dcc0769583c45b32e01b35c",
+    "url": "/static/media/dophinGame.4c30588c.jpg"
+  },
+  {
+    "revision": "0e359b6ea6ec25bfed30393a1f763d8e",
+    "url": "/static/media/tetrisblokGame.0e359b6e.jpg"
+  },
+  {
+    "revision": "98f5166bbd6980dd4c8f022591c54387",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.110b54fb558f13b87f172aba6c75d30e.js"
+  "/precache-manifest.374edfe75a8fc59a1830ca1dbb970ce2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
