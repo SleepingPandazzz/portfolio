93675a44ee1ef8ad579a8b95eeb74675c199b1b6
Tingting He <tingtinghe@Tingtings-MacBook-Pro-2.local>
2022-03-22 20:22:45 -0700

    Updates

---
+++ precache-manifest.9ecbe6b6ad7bbf5b465f55c6ba589adf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8809b15ca4fc402a090",
+    "url": "/portfolio/static/css/main.ecf7dc2b.chunk.css"
+  },
+  {
+    "revision": "f8809b15ca4fc402a090",
+    "url": "/portfolio/static/js/main.7708f081.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "14b48fe9392412509670",
+    "url": "/portfolio/static/css/2.1f1b255e.chunk.css"
+  },
+  {
+    "revision": "14b48fe9392412509670",
+    "url": "/portfolio/static/js/2.83b55812.chunk.js"
+  },
+  {
+    "revision": "cb3913666f29c38fe8de9c620f749522",
+    "url": "/portfolio/static/media/kabs.cb391366.jpg"
+  },
+  {
+    "revision": "415230e6882d8343ba1ff8eb4b7899eb",
+    "url": "/portfolio/static/media/pl4.415230e6.jpg"
+  },
+  {
+    "revision": "6c2634c23d0dd70e4b20f282f67f1578",
+    "url": "/portfolio/static/media/asteroidGame.6c2634c2.jpg"
+  },
+  {
+    "revision": "14c5eaa465acba59e7974806e53d4a85",
+    "url": "/portfolio/static/media/spaceGame.14c5eaa4.mp4"
+  },
+  {
+    "revision": "fc9b2fb347e253fa7b2b9e9e6b0b01b6",
+    "url": "/portfolio/static/media/3DGame.fc9b2fb3.jpg"
+  },
+  {
+    "revision": "4c30588c4dcc0769583c45b32e01b35c",
+    "url": "/portfolio/static/media/dophinGame.4c30588c.jpg"
+  },
+  {
+    "revision": "0e359b6ea6ec25bfed30393a1f763d8e",
+    "url": "/portfolio/static/media/tetrisblokGame.0e359b6e.jpg"
+  },
+  {
+    "revision": "f2dbb84383b78433433df6d735785970",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.374edfe75a8fc59a1830ca1dbb970ce2.js"
+  "/portfolio/precache-manifest.9ecbe6b6ad7bbf5b465f55c6ba589adf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
